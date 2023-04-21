283381f175173f0d582617416ed602728039bb51
Priyanshu Gupta <priyanshug161@gmail.com>
2023-04-21 21:22:00 +0530

    added MemeData.js

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,5 +1,14 @@
+import memesData from "../memesData"
 
 export default function Meme() {
+
+    function getMemeImage() {
+        const memesArray=memesData.data.memes
+        const randomNumber=Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        console.log(url)
+    }
+
     return (
         <main>
             <div className="form">
@@ -15,6 +24,7 @@ export default function Meme() {
                   />
                 <button
                     className="form--button"
+                    onClick={getMemeImage}
                 >
                     Get a new meme image 🖼
                 </button>
