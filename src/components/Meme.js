114588469df715807b7299465f61445bcc65430d
Priyanshu Gupta <priyanshug161@gmail.com>
2023-04-23 11:34:46 +0530

    added memeImage

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,12 +1,14 @@
+import React from 'react';
 import memesData from "../memesData"
 
 export default function Meme() {
 
+    const [memeImage,setMemeImage] = React.useState("")
+
     function getMemeImage() {
         const memesArray=memesData.data.memes
         const randomNumber=Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
-        console.log(url)
+        setMemeImage(memesArray[randomNumber].url)
     }
 
     return (
@@ -29,6 +31,7 @@ export default function Meme() {
                     Get a new meme image 🖼
                 </button>
             </div>
+            <img src={memeImage} className="meme--image" />
         </main>
     )
 }
